8e1749facb0c622589cc8866579c3055bb7821f0
HungryLearnerHere <shahpiyush933@gmail.com>
2023-08-05 18:47:27 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,8 +19,17 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: "Catalogue App",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
